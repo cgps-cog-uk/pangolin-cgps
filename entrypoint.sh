af5a60890659e50a6b94913e832231be8842a73a
Anthony Underwood <au3@sanger.ac.uk>
2020-04-09 12:10:24 +0000

    add json output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cat - > sequence.fna
 
 conda run -n pangolin-env pangolin sequence.fna > /dev/null 2> /dev/null
 
-cat analysis/lineage_report.csv
+cat analysis/lineage_report.csv | python3 csv2json.py
