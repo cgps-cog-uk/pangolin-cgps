d4d0993fd75372b0b3d30830bf5aa9960228e18e
Anthony Underwood <au3@sanger.ac.uk>
2020-04-13 21:30:48 +0000

    update to official pangolin repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cat - > sequence.fna
 
 conda run -n pangolin pangolin sequence.fna > /dev/null 2> /dev/null
 
-cat analysis/lineage_report.csv | python3 csv2json.py
+cat lineage_report.csv | python3 csv2json.py
