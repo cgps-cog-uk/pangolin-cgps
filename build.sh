d2d9a34b04f9bbaa955cd7491d759cd6da8d03d3
Anthony Underwood <au3@sanger.ac.uk>
2020-05-02 16:42:34 +0000

    use correct tag for image v3

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ lineages_version=${1}_${2}
 echo "Building ${lineages_version}."
 
 cd code
-docker build --rm --build-arg VERSION=${code_version} -t registry.gitlab.com/cgps/lineages-code:${code_version} .
+docker build --rm --build-arg VERSION=${code_version} -t registry.gitlab.com/cgps/cog-uk/lineages-code:${code_version} .
 cd ../data
-docker build --rm --build-arg VERSION=${data_version} -t registry.gitlab.com/cgps/lineages-data:${data_version} .
+docker build --rm --build-arg VERSION=${data_version} -t registry.gitlab.com/cgps/cog-uk/lineages-data:${data_version} .
 cd ..
-docker build --rm --build-arg CODE_VERSION=${code_version} --build-arg DATA_VERSION=${data_version} -t registry.gitlab.com/cgps/lineages:${lineages_version} .
+docker build --rm --build-arg CODE_VERSION=${code_version} --build-arg DATA_VERSION=${data_version} -t registry.gitlab.com/cgps/cog-uk/lineages:${lineages_version} .
