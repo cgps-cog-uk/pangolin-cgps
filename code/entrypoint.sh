8be57e05c6804eb22ffea22040d73012557493ff
Anthony Underwood <au3@sanger.ac.uk>
2020-05-09 06:58:04 +0000

    allow stderr

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 
 cat - > sequence.fna
 
-conda run -n pangolin pangolin --panGUIlin -d /data/ sequence.fna > /dev/null 2>&1
+conda run -n pangolin pangolin --panGUIlin -d /data/ sequence.fna > /dev/null
 
 python3 csv_reports_to_json.py lineage_report.csv global_lineage_information.csv
