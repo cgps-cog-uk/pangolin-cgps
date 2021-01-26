7e348806454ca6d099b097da4956a18d86f8cf77
Anthony Underwood <au3@sanger.ac.uk>
2021-01-26 17:55:51 +0000

    update repo names

--- single_stage_build/build.sh
+++ single_stage_build/build.sh
@@ -1,4 +1,4 @@
 pangolin_version=${1}
 
-docker build --rm --build-arg VERSION=${pangolin_version} -t registry.gitlab.com/cgps/cog-uk/lineages:${pangolin_version} single_stage_build
-docker push registry.gitlab.com/cgps/cog-uk/lineages:${pangolin_version}
+docker build --rm --build-arg VERSION=${pangolin_version} -t registry.gitlab.com/cgps/cog-uk/pangolin:${pangolin_version} single_stage_build
+docker push registry.gitlab.com/cgps/cog-uk/pangolin:${pangolin_version}
