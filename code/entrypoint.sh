dd573aa1c994a19ad664a52abe84e4f7fff9b389
Anthony Underwood <au3@sanger.ac.uk>
2021-01-26 17:21:49 +0000

    Fail on more than 1 input sequences in the fasta file

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-set -e -o pipefail
+set -eu -o pipefail
 
 cat - > sequence.fna
+
+NUM_SEQUENCES=$(grep -E "^>" sequence.fna | wc -l)
+if [[ $NUM_SEQUENCES -gt 1 ]]; then echo "Only expected 1 sequence, not ${NUM_SEQUENCES}" 2>&1; exit 1; fi
 
 conda run -n pangolin pangolin --panGUIlin -d /data/ sequence.fna > /dev/null 2> /dev/null
 
